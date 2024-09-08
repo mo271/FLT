24b6223b81efba8879d78d6b82d37890d02caab1
Moritz Firsching <firsching@google.com>
2024-09-09 00:04:02 +0200

    half of MatrixRing.isCentralSimple

--- FLT/for_mathlib/IsCentralSimple.lean
+++ FLT/for_mathlib/IsCentralSimple.lean
@@ -3,8 +3,9 @@ Copyright (c) 2024 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard
 -/
-
 import Mathlib.Algebra.Quaternion -- probably get away with less
+import Mathlib.RingTheory.HopfAlgebra
+import Mathlib.Algebra.Lie.OfAssociative
 
 
 /-!
@@ -31,7 +32,37 @@ structure IsCentralSimple
 variable (K : Type u) [Field K]
 
 theorem MatrixRing.isCentralSimple (ι : Type v) (hι : Fintype ι) (hnonempty : Nonempty ι) [DecidableEq ι] :
-    IsCentralSimple K (Matrix ι ι K) := sorry
+    IsCentralSimple K (Matrix ι ι K) := by
+  constructor
+  · intro d hd
+    obtain ⟨h1, _, _ , _⟩ := hd
+    have ⟨k, hk⟩ : ∃ k : K, d = Matrix.diagonal (fun i => k) := by
+      use d hnonempty.some hnonempty.some
+      have h_diag : ∀ i j : ι, d i i = d j j := by
+        intro i j
+        set M := (Matrix.stdBasisMatrix i j (1 : K))
+        specialize h1 M
+        have eq_entry : (d * M) i j =
+            (M * d) i j := by
+          rw [h1]
+        simp [M] at eq_entry
+        exact eq_entry
+      ext i j
+      by_cases hij : i = j
+      · simp only [hij, Matrix.diagonal, Matrix.of_apply, ↓reduceIte]
+        exact h_diag j hnonempty.some
+      · simp at hij
+        simp only [ne_eq, hij, not_false_eq_true, Matrix.diagonal_apply_ne]
+        let M := Matrix.stdBasisMatrix j j ( (1 : K))
+        specialize h1 M
+        have eq_entry : (d * M) i j = (M * d) i j := by
+          rw [h1]
+        simp only [Matrix.diagonal_one, M, Matrix.StdBasisMatrix.mul_right_apply_same, mul_one,
+            Matrix.StdBasisMatrix.mul_left_apply_of_ne _ _ _  _ _ hij] at eq_entry
+        ring_nf at eq_entry
+        exact eq_entry
+    exact Exists.intro k hk
+  · sorry
 
 namespace IsCentralSimple
 
@@ -51,10 +82,12 @@ variable (D : Type v) [Ring D] [Algebra K D] (h : IsCentralSimple K D)
 
 open scoped TensorProduct
 
--- lemma baseChange (L : Type w) [Field L] [Algebra K L] : IsCentralSimple L (L ⊗[K] D) := sorry
+lemma baseChange (L : Type w) [Field L] [Algebra K L] : IsCentralSimple L (L ⊗[K] D) := by
+    constructor
+    · sorry
+    · sorry
 
 end IsCentralSimple
-
 -- restrict to 4d case
 -- theorem exists_quaternionAlgebra_iso (hK : (2 : K) ≠ 0) :
 --     ∃ a b : K, Nonempty (D ≃ₐ[K] ℍ[K, a, b]) := sorry
